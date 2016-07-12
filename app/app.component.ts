e33c04b2b49ced09d35b19e9b63cbbbe70e5d9e6
Denis Michailov <denis.michailov@gmail.com>
2016-07-12 15:46:28 +1000

    Adding elements

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,12 +3,22 @@ import {Component} from '@angular/core';
 @Component({
 	selector: 'todo-app',
 	templateUrl: './app/app.component.html',
-	styleUrls: ['./app/app.components.css']
+	styleUrls: ['./app/app.component.css']
 })
 
 export class AppComponent {
 	title: string;
+	todos: string[];
+
 	constructor() {
-		this.title = 'Main 2Do'
+		this.title = 'Main 2';
+		this.todos = [];
+
 	}
+
+	addTodo(event: any, input: HTMLInputElement) {
+		this.todos.push(input.value);
+		input.value = '';
+	}
+
 }
