2964ff20afac30180168d611a4c48a0a39aa45d9
Denis Michailov <denis.michailov@gmail.com>
2016-07-12 14:41:01 +1000

    + add form

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,12 +2,13 @@ import {Component} from '@angular/core';
 
 @Component({
 	selector: 'todo-app',
-	templateUrl: './app/app.component.html'
+	templateUrl: './app/app.component.html',
+	styleUrls: ['./app/app.components.css']
 })
 
 export class AppComponent {
 	title: string;
 	constructor() {
-		this.title = 'Angular 2Do'
+		this.title = 'Main 2Do'
 	}
 }
